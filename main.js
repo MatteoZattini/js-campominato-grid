3ee882e63f972b8fa5aa6d6992378715092e5372
MatteoZattini <matteozattini@hotmail.com>
2024-04-30 16:36:35 +0200

    m4 funzione per la creazione del quadrato

--- main.js
+++ main.js
@@ -4,16 +4,29 @@ let griglia = document.getElementById("grid")
 // creo un ciclo per stampare in pagina i quadrati
 for (let i = 1; i <= 100; i++) {
 
-    let square = document.createElement("div");
-    square.classList.add("square");
-    griglia.append(square);
+    // let square = document.createElement("div");
+    // square.classList.add("square");
+    // griglia.append(square);
+
+    // chiamo la funzione
+    let elemento = mySquare(i);
 
     // creo un event listener che aggiunga sia il numero che un bg color allo
     // square selezionato
-    square.addEventListener("click", function(){
+    elemento.addEventListener("click", function(){
         
-        square.innerText = i;
-        square.classList.toggle("evidenziata")
+        elemento.innerText = i;
+        elemento.classList.toggle("evidenziata")
 
     })
+
+    griglia.append(elemento);
+
+}
+
+// creo una funzione per la creazione del quadrato
+function mySquare (contenuto) {
+    let square = document.createElement("div");
+    square.classList.add("square");
+    return square;
 }
