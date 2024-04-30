19b4f2a9ee980aac676f04157d0d2b2b4e56edb4
MatteoZattini <matteozattini@hotmail.com>
2024-04-30 17:14:39 +0200

    bonus1 e bonus2

--- main.js
+++ main.js
@@ -1,35 +1,39 @@
-// mi credo una variabile per richiamare l'id grid
-let griglia = document.getElementById("grid")
+let myButton = document.getElementById("bottone")
+myButton.addEventListener("click", function(){
 
-// creo un ciclo per stampare in pagina i quadrati
-for (let i = 1; i <= 100; i++) {
-
-    // let square = document.createElement("div");
-    // square.classList.add("square");
-    // griglia.append(square);
-
-    // chiamo la funzione
-    let quadrato = mySquare(i);
-
-    // creo un event listener che aggiunga sia il numero che un bg color allo
-    // square selezionato
-   
-
-    griglia.append(quadrato);
-
-}
-
-// creo una funzione per la creazione del quadrato
-function mySquare (contenuto) {
-    let square = document.createElement("div");
-    square.classList.add("square");
-
-    square.addEventListener("click", function(){
-        
-        square.innerText = contenuto;
-        square.classList.toggle("evidenziata")
-
-    })
-
-    return square;
-}
+    // mi credo una variabile per richiamare l'id grid
+    let griglia = document.getElementById("grid")
+    
+    // creo un ciclo per stampare in pagina i quadrati
+    for (let i = 1; i <= 100; i++) {
+    
+        // let square = document.createElement("div");
+        // square.classList.add("square");
+        // griglia.append(square);
+    
+        // chiamo la funzione
+        let quadrato = mySquare(i);
+    
+        // creo un event listener che aggiunga sia il numero che un bg color allo
+        // square selezionato
+       
+    
+        griglia.append(quadrato);
+    
+    }
+    
+    // creo una funzione per la creazione del quadrato
+    function mySquare (contenuto) {
+        let square = document.createElement("div");
+        square.classList.add("square");
+    
+        square.addEventListener("click", function(){
+            
+            square.innerText = contenuto;
+            square.classList.toggle("evidenziata")
+    
+        })
+    
+        return square;
+    }
+})
